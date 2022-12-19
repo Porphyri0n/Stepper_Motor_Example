0896cdcb1be37cd97618f3d73e56f618d11c8a1c
Porphyri0n <119479540+Porphyri0n@users.noreply.github.com>
2022-12-19 22:57:02 +0300

    Update Stepper_motor.ino

--- Stepper_motor.ino
+++ Stepper_motor.ino
@@ -1,23 +1,23 @@
-#include<AccelStepper.h>
+#include<AccelStepper.h>                    //kütüphane
 
-AccelStepper stepperX(1,8,9);
-AccelStepper stepperY(1,10,11);
+AccelStepper stepperX(1,8,9);               //X motorunun kurulumu(Sürücüye göre parantez içi değerleri değişecek)
+AccelStepper stepperY(1,10,11);             //Y motorunun kurulumu(Sürücüye göre parantez içi değerleri değişecek)
 int motion[] = {};
 
-int sensor1Val;
-int sensor2Val;
-int buttonInputSAG = 2;
-int buttonInputSOL = 3;
-int buttonInputSTART = 4;
-int buttonInputSTOP = 5;
-int buttonInputRESET = 6;
-bool state = false;
+int sensor1Val;                             //Eklenecek sensörlerden birinin input değeri
+int sensor2Val;                             //Eklenecek sensörlerden birinin input değeri
+int buttonInputSAG = 2;                     //Sağ buton pini
+int buttonInputSOL = 3;                     //Sol buton pini
+int buttonInputSTART = 4;                   //Start buton pini
+int buttonInputSTOP = 5;                    //Stop buton pini
+int buttonInputRESET = 6;                   //Reset buton pini
+bool state = false;                         //Motion başladı mı başlamadı mı kontrolü
 
 
 void setup(){
   
   //X Motoru
-  stepperX.setMaxSpeed(1000);
+  stepperX.setMaxSpeed(1000);               //Motor kurulumu
   stepperX.setAcceleration(1000);
   stepperX.disableOutputs();
 
@@ -31,7 +31,7 @@ void loop(){
   
 }
 
-void ResetMotion(){
+void ResetMotion(){            //Hareketi hafızadan siler
   if(state == true){
     memset(motion, 0, sizeof(motion));
     state = false;
@@ -40,17 +40,17 @@ void ResetMotion(){
 
 
 
-void OneStepR(){
+void OneStepR(){               //Sağa doğru bir adım motoru sürer
   stepperX.moveTo(10);
   stepperX.runToPosition();
 }
 
-void OneStepL(){
+void OneStepL(){               //Sola doğru bir adım motoru sürer
   stepperX.moveTo(-10);
   stepperX.runToPosition();
 }
 
-void MotionSave(){
+void MotionSave(){             //Hareketi kaydeder
   if (digitalRead(buttonInputSAG) == HIGH){
     OneStepR();
     motion[sizeof(motion)] = 10;
@@ -62,11 +62,17 @@ void MotionSave(){
   }
 }
 
-void RunMotion(){
+void RunMotion(){            //Hareketi başlatır
   for(int counter = 0; counter < int(sizeof(motion)); counter++){
     stepperX.moveTo(motion[counter]);
     stepperX.runToPosition();
   }
 }
+
+
+
+
+
+
 
 
