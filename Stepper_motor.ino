dab6516056cc55c93521658d558003321f149217
Porphyri0n <119479540+Porphyri0n@users.noreply.github.com>
2022-12-24 23:49:14 +0300

    Update Stepper_motor.ino

--- Stepper_motor.ino
+++ Stepper_motor.ino
@@ -1,7 +1,17 @@
 #include<AccelStepper.h>                    //kütüphane
+#define HALFSTEP 8
+#define motorXPin1 9
+#define motorXPin2 10
+#define motorXPin3 11
+#define motorXPin4 12
+//#define motorYPin4 12
+//#define motorYPin4 12
+//#define motorYPin4 12
+//#define motorYPin4 12
 
-AccelStepper stepperX(2000,9,10,11,12);               //X motorunun kurulumu(Sürücüye göre parantez içi değerleri değişecek)
-//AccelStepper stepperY(1,10,11);             //Y motorunun kurulumu(Sürücüye göre parantez içi değerleri değişecek)
+
+AccelStepper stepperX(HALFSTEP,motorXPin1,motorXPin2,motorXPin3,motorXPin4);               //X motorunun kurulumu(Sürücüye göre parantez içi değerleri değişecek)
+//AccelStepper stepperY(HALFSTEP,motorYPin1,motorYPin2,motorYPin3,motorYPin4);               //Y motorunun kurulumu(Sürücüye göre parantez içi değerleri değişecek)
 int motion[] = {};
 
 int sensor1Val;                             //Eklenecek sensörlerden birinin input değeri
@@ -15,10 +25,11 @@ int buttonInputEND = 7;                     //End buton pini
 //int buttonInputToHome = 13;               //To home buton pini
 //int buttonInputToEnd = 13;                //To End buton pini
 bool state = false;                         //Motion başladı mı başlamadı mı kontrolü
-int endpos;
+int endpos = 1000;
 int homepos = 0;
 
 void setup(){
+  Serial.begin(9600);
   pinMode(buttonInputSAG,INPUT);
   pinMode(buttonInputSOL,INPUT);
   pinMode(buttonInputSTART,INPUT);
@@ -30,7 +41,6 @@ void setup(){
   Serial.println("Motor Testi");            //Motor testi
   stepperX.setMaxSpeed(1000);               
   stepperX.setAcceleration(1000);
-  stepperX.disableOutputs();
 
   //Y Motoru
   //stepperY.setMaxSpeed(1000);
@@ -39,49 +49,54 @@ void setup(){
 }
 void loop(){
   if(state == false){
+    Serial.println("Kayit Modu");
+    Direction();
     stepperX.enableOutputs();
-    Right();
-    Left();
-    stepperX.run();
+    stepperX.runSpeed();
     Home();
     End();
   }
   else{
+    Serial.println("Motion modu");
     Motion();
   }
   if(digitalRead(buttonInputSTART)==HIGH){
+    Serial.println("Motion moduna geçildi");
     state = true;
     delay(300);
   }
 
   stepperX.run();
+
 }
 
-
-void Right(){
-  if(digitalRead(buttonInputSAG) == HIGH){
-  stepperX.moveTo(1);
+//Sağ Sol yaptırma işlemi
+void Direction(){
+  if(digitalRead(buttonInputSAG) == HIGH && digitalRead(buttonInputSOL) == LOW){
+    stepperX.setSpeed(500);
+  }
+  else if(digitalRead(buttonInputSOL) == HIGH && digitalRead(buttonInputSAG) == LOW){
+    stepperX.setSpeed(-500);
+  }
+  else{
+    stepperX.setSpeed(0);
   }
 }
 
-void Left(){
-  if(digitalRead(buttonInputSAG) == HIGH){
-  stepperX.moveTo(-1);
-  }
-  
-}
+//Home pozisyonu ayarlama
 void Home(){
   if(digitalRead(buttonInputHOME)==HIGH){
-    stepperX.setCurrentPosition(0);
+    stepperX.setCurrentPosition(homepos);
   }
 }
-
+//End pozisyonu ayarlama
 void End(){
   if(digitalRead(buttonInputEND)==HIGH){
     endpos = stepperX.currentPosition();
   }
   
 }
+//Kaydedilen konumlar üzerinden hareketi başlatma
 void Motion(){
     while (stepperX.currentPosition() != 0){
       stepperX.moveTo(0);
@@ -91,6 +106,7 @@ void Motion(){
     }
 }
 
+//Programı başa alma
 void Reset(){
   state = false;
   stepperX.disableOutputs();
